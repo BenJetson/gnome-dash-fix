503301efc6d99d394da8803f392b89f6a7e4904c
Pranesh Sekar <pranesh2014@gmail.com>
2023-08-27 03:23:54 -0400

    Add Kali Linux Tools

--- interactive.py
+++ interactive.py
@@ -46,6 +46,29 @@ DEFAULTS = [Folder("Accessories", ['Utility']),
             Folder("Universal Access", ['Accessibility']),
             Folder("Wine", ['Wine', 'X-Wine', 'Wine-Programs-Accessories'])]
 
+KALI_LINUX_DEFAULTS = [
+            Folder("001 - Identify", ['001-identify']),
+            Folder("002 - Protect", ['002-protect']),
+            Folder("003 - Detect", ['003-detect']),
+            Folder("004 - Respond", ['004-respond']),
+            Folder("005 - Recover", ['005-recover']),
+            Folder("01 - Information Gathering", ['01-info-gathering', '01-01-dns-analysis', '01-02-identify-live-hosts', '01-03-ids-ips-identification', '01-04-network-scanners', '01-07-osint-analysis', '01-08-route-analysis', '01-10-smb-analysis', '01-11-smtp-analysis', '01-12-snmp-analysis', '01-13-ssl-analysis']),
+            Folder("02 - Vulnerability Analysis",   ['02-vulnerability-analysis', '02-01-cisco-tools', '02-02-fuzzers', '02-03-voip-tools', '02-05-nessus', '02-06-openvas', '02-07-stress-testing', '02-08-gvm']),
+            Folder("03 - Web Application Analysis",   ['03-webapp-analysis', '03-01-cms-identification', '03-04-web-crawlers', '03-05-web-vulnerability-scanners', '03-06-web-application-proxies']),
+            Folder("04 - Database Assessment", ['04-database-assessment']),
+            Folder("05 - Password Attacks",   ['05-password-attacks', '05-01-online-attacks', '05-02-offline-attacks', '05-04-pass-hash', '05-05-profile']),
+            Folder("06 - Wireless Attacks",   ['06-wireless-attacks', '06-01-wireless-tools', '06-02-bluetooth-tools', '06-03-rfid-nfc-tools', '06-04-other-wireless', '06-05-radio-tools']),
+            Folder("07 - Reverse Engineering", ['07-reverseengineer']),
+            Folder("08 - Exploitation Tools",   ['08-exploitation-tools', '08-01-metasploit-framework']),
+            Folder("09 - Sniffing & Spoofing",   ['09-sniffing-spoofing', '09-01-network-sniffers', '09-02-network-spoofing']),
+            Folder("10 - Post Exploitation",   ['10-maintaining-access', '10-01-os-backdoors', '10-02-tunneling', '10-03-web-backdoors', '10-04-command-control']),
+            Folder("11 - Forensics",   ['11-forensics', '11-01-network-forensics', '11-03-digital-forensics', '11-04-forensic-analysis-tools', '11-05-forensic-carving-tools', '11-07-forensic-imaging-tools', '11-08-forensic-suites', '11-11-pdf-forensics-tools']),
+            Folder("12 - Reporting Tools", ['12-reporting']),
+            Folder("13 - Social Engineering Tools", ['13-social-engineering-tools']),
+            Folder("14 - System Services",   ['14-services', '14-01-gpsd-service', '14-02-httpd-service', '14-03-mysqld-service', '14-04-pcscd-service', '14-06-sshd-service', '14-07-radius-service', '14-08-beef-service', '14-09-metasploit-service', '14-10-dradis-service', '14-11-openvas-service', '14-12-xplico-service', '14-13-gvm-service', '14-14-faraday-service', '14-15-nessus-service', '14-16-defectdojo-service']),
+            Folder("15 - Kali & OffSec Links", ['15-kali-offsec-links']),
+            ]
+
 def ynQuery(question):
     while True:
         print()
@@ -66,14 +89,15 @@ def menu():
         print()
         print("""---Main Menu---
         1. Set folders to default categories.
-        2. Customize folders and categories before applying.
-        3. Remove all existing folders from dashboard.
-        4. Cancel and exit.""")
+        2. Sort Kali Linux Applications.
+        3. Customize folders and categories before applying.
+        4. Remove all existing folders from dashboard.
+        5. Cancel and exit.""")
         print()
 
         choice = input("What do you want to do? >>> ")
 
-        if choice.isnumeric() and int(choice) in range(1, 5):
+        if choice.isnumeric() and int(choice) in range(1, 6):
             return int(choice)
         else:
             os.system("clear")
@@ -138,6 +162,29 @@ def setSorted():
         print()
         doReset()
         writeSettings(DEFAULTS)
+        os.system("clear")
+        for i in range(2): print()
+        print("Your apps dashboard is now sorted by the predefined categories.")
+    else:
+        print()
+        print("Sort not performed. Exiting.")
+        sys.exit()
+
+def sortKaliLinuxTools():
+    os.system("clear")
+    for i in range(2): print()
+    print("""WARNING! This will erase ALL folders you have
+        in your apps dashboard and sort your apps
+        based upon a predefined set of basic
+        categories. 
+
+        The folders erased will include user-created
+        folders as well as any created by other means.""")
+
+    if ynQuery("Are you sure you wish to do this?"):
+        print()
+        doReset()
+        writeSettings(KALI_LINUX_DEFAULTS)
         os.system("clear")
         for i in range(2): print()
         print("Your apps dashboard is now sorted by the predefined categories.")
@@ -327,10 +374,12 @@ choice = menu()
 if choice == 1:
     setSorted()
 elif choice == 2:
+    sortKaliLinuxTools()
+elif choice == 3:
     customBuild()
     #print("Sorry! This feature is currently planned, but not yet implemented.")
-elif choice == 3:
-    resetAll()
 elif choice == 4:
+    resetAll()
+elif choice == 5:
     print("Goodbye.")
     sys.exit()
